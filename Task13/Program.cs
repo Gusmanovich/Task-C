1da14106604bd00fca4af92bfb8952a2627fd0b2
Almir <almir@example.com>
2022-05-23 18:11:22 +0300

    Решение задачи 29 с отрицательными значениями

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,10 +5,28 @@
 // 32679 -> 6
 
 
-// int number = new Random().Next(10, 10000);
-// Console.WriteLine(number);
-int number = 645;
-string str = number.ToString();
-//int sot = (number/100) % 10;
-if (sot == 0) Console.WriteLine("Третьей цифры нет");
-else Console.WriteLine("Третья цифра - " + sot);
+int GetThirdNumber(int inputNumber)
+{
+    int result = 0;
+    while (inputNumber > 999) //Делится и забываются числа больше 999
+    {
+        inputNumber /= 10;
+    }
+    result = inputNumber % 10;
+    return result;
+}
+
+Console.Write("Введите число:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number > 99)
+{
+    int thirdNumber = GetThirdNumber(number);
+    Console.WriteLine($"Третье число {thirdNumber}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+
+}
